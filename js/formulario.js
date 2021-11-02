20239881621bc3321a61198693d43bc395560602
orlando-yerter <53948381+orlando-yerter@users.noreply.github.com>
2021-11-02 01:47:19 -0300

    segundo commit

--- js/formulario.js
+++ js/formulario.js
@@ -1,22 +1,28 @@
+
+
 function validar(){
-    var form =document.form;
+    var form =document.form;   
       if(form.nombre.value==0){
         alert("el nombre esta vacio");
         form.nombre.value= "";
         form.nombre.focus();
         return 0;
       }
-	
-	
-      else if(form.email.value==0){
+      else if(form.apellido.value==0){
+        alert("apellido  esta vacio");
+        form.apellido.value= "";
+        form.apellido.focus();
+        return 0;
+      }    
+     
+      else if(form.mail.value==0){
         alert("el mail  esta vacio");
-        form.email.value= "";
-        form.email.focus();
-        return false;
-    }
-         return true;
-   alert("los datos se han enviados correctamente");
-   form.submit();
+        form.mail.value= "";
+        form.mail.focus();
+        return 0;
+    }   
+        alert(" todos los datos se han validados correctamente");
+        document.form.submit();
 }
 
 
